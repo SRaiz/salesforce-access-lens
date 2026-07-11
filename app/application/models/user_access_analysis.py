4567b4bc26300327ad83a8f687db70e94b51b368
Sidharth Raizada <sidharthraizada07@gmail.com>
2026-07-11 23:43:29 +0100

    feat(analysis): add object and field access capabilities

--- app/application/models/user_access_analysis.py
+++ app/application/models/user_access_analysis.py
@@ -57,6 +57,105 @@ class UserAccessAnalysis:
             * self.assigned_permission_sets
         ]
         
+    def get_object_permission( self, object_name: str ) -> ObjectPermission | None:
+        return next(
+            filter(
+                lambda permission: permission.sobject_type == object_name, 
+                self.object_permissions
+            ), 
+            None
+        )
+        
+    def can_read_object( self, object_name: str ) -> bool:
+        permission = self.get_object_permission( object_name )
+
+        return (
+            permission.can_read
+            if permission is not None
+            else False
+        )
+
+
+    def can_create_object( self, object_name: str ) -> bool:
+        permission = self.get_object_permission( object_name )
+
+        return (
+            permission.can_create
+            if permission is not None
+            else False
+        )
+
+
+    def can_edit_object( self, object_name: str ) -> bool:
+        permission = self.get_object_permission( object_name )
+
+        return (
+            permission.can_edit
+            if permission is not None
+            else False
+        )
+
+
+    def can_delete_object( self, object_name: str ) -> bool:
+        permission = self.get_object_permission( object_name )
+
+        return (
+            permission.can_delete
+            if permission is not None
+            else False
+        )
+        
+    def get_field_permission(
+        self, 
+        object_name : str, 
+        field_name  : str
+    ) -> FieldPermission | None:
+        
+        return next(
+            filter(
+                lambda permission: (
+                    permission.object_name == object_name 
+                    and permission.field_name == field_name
+                ), 
+                self.field_permissions
+            ), 
+            None
+        )
+        
+    def can_read_field(
+        self, 
+        object_name : str, 
+        field_name  : str
+    ) -> bool:
+        
+        permission = self.get_field_permission(
+            object_name, 
+            field_name
+        )
+        
+        return (
+            permission.can_read 
+            if permission is not None 
+            else False
+        )
+        
+    def can_edit_field(
+        self, 
+        object_name : str, 
+        field_name  : str
+    ) -> bool:
+        
+        permission = self.get_field_permission(
+            object_name, 
+            field_name
+        )
+        
+        return (
+            permission.can_edit 
+            if permission is not None 
+            else False
+        )
+        
     def __repr__( self ):
         return (
             "\n"
